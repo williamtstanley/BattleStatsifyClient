374e93e3506f347d9fb74242c5ade588db392daf
Travis Stanley <travisstanley@Admins-MacBook-Air.local>
2017-08-19 05:40:13 -0700

    badly create a logo for the app

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Logo, TitleText } from '../../components'
-import logo from '../../media/logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <Logo src={logo} className="App-logo" alt="logo" />
+          <Logo />
           <TitleText>BattleStatsify</TitleText>
         </div>
         <p className="App-intro">
