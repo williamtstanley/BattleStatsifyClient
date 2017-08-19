a42be936366ffc81d0c71b49aaf01a156b67474e
Travis Stanley <travisstanley@Admins-MacBook-Air.local>
2017-08-18 22:00:05 -0700

    restructure components into folders

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../../media/logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to BattleStatsify</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          here goes content 
         </p>
       </div>
     );
